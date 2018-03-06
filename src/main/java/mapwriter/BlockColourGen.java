c303ea1efdfa00239e1f194386def476b06f8ffa
Tyler Hancock <darklime@live.ca>
2018-03-05 17:57:29 -0700

    Processing of textures is now handled seperately.

--- src/main/java/mapwriter/BlockColourGen.java
+++ src/main/java/mapwriter/BlockColourGen.java
@@ -17,30 +17,38 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.ColorizerFoliage;
 import net.minecraft.world.ColorizerGrass;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 public class BlockColourGen {
     
     public static final Map<TextureAtlasSprite, Integer> COLOUR_MAP = new HashMap<>();
     
+    public static Map<String, TextureAtlasSprite> getTextures(TextureMap map) {
+        
+        return ReflectionHelper.getPrivateValue(TextureMap.class, map, "mapRegisteredSprites");
+    }
+    
     public static void genBlockColours (BlockColours bc) {
 
         Logging.log("generating block map colours from textures");
-
-        final int terrainTextureId = Minecraft.getMinecraft().renderEngine.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).getGlTextureId();
-
-        if (terrainTextureId == 0) {
-            
-            Logging.log("error: could get terrain texture ID");
-            return;
-        }
         
+        final int terrainTextureId = Minecraft.getMinecraft().renderEngine.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).getGlTextureId();
         final Texture terrainTexture = new Texture(terrainTextureId);
 
-        int e_count = 0;
-        int b_count = 0;
-        int s_count = 0;
+        long preTime = System.currentTimeMillis();        
+        int textures = 0;
+        
+        for (final TextureAtlasSprite sprite : getTextures(Minecraft.getMinecraft().getTextureMapBlocks()).values()) {
+            
+            COLOUR_MAP.put(sprite, getIconMapColour(sprite, terrainTexture));
+            textures++;
+        }
+        
+        Logging.log("Processed %d textures. Took %dms", textures, System.currentTimeMillis() - preTime);
 
         long start = System.currentTimeMillis();
+        int blocks = 0;
+        int exceptions = 0;
         
         for (final Block block : Block.REGISTRY) {
 
@@ -52,37 +60,24 @@ public class BlockColourGen {
                         
                         final TextureAtlasSprite icon = Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes().getTexture(state);
                         
-                        int blockColour = 0;
-                        
                         if (icon != null) {
 
-
-                            if (COLOUR_MAP.containsKey(icon)) {
-                                
-                                blockColour = COLOUR_MAP.get(icon);
-                                s_count++;
-                            }
-                            
-                            else {
-                                
-                                blockColour = getIconMapColour(icon, terrainTexture);
-                                b_count++;
-                                COLOUR_MAP.put(icon, blockColour);
-                            }
-                            
+                            final int blockColour = COLOUR_MAP.get(icon);                            
                             bc.setColour(block.delegate.name().toString(), String.valueOf(block.getMetaFromState(state)), blockColour);
+                            blocks++;
                         }
                     }
                     
                     catch (Exception e) {
                         
+                        exceptions++;
                         e.printStackTrace();
                     }
                 }
             }
         }
 
-        Logging.log("processed %d block textures, %d skipped, %d exceptions", b_count, s_count, e_count);
+        Logging.log("Processed %d blocks. Had %d exceptions. Took %dms", blocks, exceptions, System.currentTimeMillis() - start);
         System.out.println("Time: " + (System.currentTimeMillis() - start));
 
         genBiomeColours(bc);
