c81e58624af378c41986609d60b700bb0539dda4
Tyler Hancock <darklime@live.ca>
2018-03-05 18:02:29 -0700

    Deprecated the logging class.

--- src/main/java/mapwriter/util/Logging.java
+++ src/main/java/mapwriter/util/Logging.java
@@ -2,29 +2,40 @@ package mapwriter.util;
 
 import mapwriter.forge.MwForge;
 
+/**
+ * THIS CLASS IS DEPRECATED BECAUSE IT IS POINTLESS OVERHEAD. PLEASE JUST USE THE LOGGER
+ * DIRECTLY. USE {} TO REFERENCE AN ARGUMENT INSTEAD OF %_
+ */
+@Deprecated
 public class Logging {
+
+    @Deprecated
     public static void debug (String s, Object... args) {
 
-        MwForge.logger.debug(String.format(s, args));
+        MwForge.logger.debug(s, args);
     }
 
+    @Deprecated
     public static void log (String s, Object... args) {
 
-        logInfo(String.format(s, args));
+        logInfo(s, args);
     }
 
+    @Deprecated
     public static void logError (String s, Object... args) {
 
-        MwForge.logger.error(String.format(s, args));
+        MwForge.logger.error(s, args);
     }
 
+    @Deprecated
     public static void logInfo (String s, Object... args) {
 
-        MwForge.logger.info(String.format(s, args));
+        MwForge.logger.info(s, args);
     }
 
+    @Deprecated
     public static void logWarning (String s, Object... args) {
 
-        MwForge.logger.warn(String.format(s, args));
+        MwForge.logger.warn(s, args);
     }
 }
