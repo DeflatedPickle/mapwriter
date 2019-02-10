dd2be20907d729ee1836e761d6d20bd78508a240
Виктор <radviger@gmail.com>
2019-02-10 02:06:01 +0200

    Fixed default small map position

--- src/main/java/mapwriter/config/SmallMapModeConfig.java
+++ src/main/java/mapwriter/config/SmallMapModeConfig.java
@@ -30,8 +30,8 @@ public class SmallMapModeConfig extends MapModeConfig {
         this.biomeModeDef = TEXT_MODE[0];
         this.playerArrowSizeDef = 4;
         this.markerSizeDef = 3;
-        this.xPosDef = 0;
-        this.yPosDef = 0;
+        this.xPosDef = 10;
+        this.yPosDef = 10;
         this.heightPercentDef = 30;
         this.widthPercentDef = 30;
 
